7b2bbaa0c8f1f678f166965eb5bcc9051409c2e0
natethegreat84 <144256165+natethegreat84@users.noreply.github.com>
2025-08-22 14:13:17 -0500

    Update te.py

--- src/pages/te.py
+++ src/pages/te.py
@@ -11,7 +11,7 @@ import nfl_data_py as nfl
 import dash_bootstrap_components as dbc
 
 # bring in nfl play data for the previous seasons
-player_stats = nfl.import_weekly_data(years=[2019,2020,2021,2022,2023], columns=['player_display_name', 'position', 'season_type', 'season', 'week', 'recent_team', 'opponent_team'
+player_stats = nfl.import_weekly_data(years=[2019,2020,2021,2022,2023,2024], columns=['player_display_name', 'position', 'season_type', 'season', 'week', 'recent_team', 'opponent_team'
                                                                            ,'carries', 'rushing_yards',
                                                                             'rushing_tds', 'rushing_fumbles', 'rushing_first_downs', 
                                                                             'receptions', 'targets', 'receiving_yards', 'receiving_tds', 'receiving_air_yards',
